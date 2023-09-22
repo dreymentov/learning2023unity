e9927ca887340f88278981d380227a554f18ec96
YHWH <dreymentov@mail.ru>
2023-09-22 22:01:51 +0300

    0.2v
    add UI and method

---
+++ unityFolder/Assets/Settings.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    bool isFullScreen;
+
+    public AudioMixer am;
+
+    Resolution[] rsl;
+    List<string> resolutions;
+    public Dropdown dropdown;
+
+    public void FullScreenToggle()
+    {
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void AudioVolume(float sliderValue)
+    {
+        am.SetFloat("masterVolume", sliderValue);
+    }
+
+    public void Quality(int q)
+    {
+        QualitySettings.SetQualityLevel(q);
+    }
+
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width + "x" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutions);
+    }
+
+    public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
+    }
+}
